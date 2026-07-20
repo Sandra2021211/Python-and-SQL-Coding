8f25482478e03204c6fb3d4810b5c360ef1f1e24
Sandra2021211 <87116316+Sandra2021211@users.noreply.github.com>
2026-07-20 03:17:58 +0000

    added sql11 program

--- SQL/Leetcode/WindowFunctions/CumulativeSalaryOfAnEmployee.sql
+++ SQL/Leetcode/WindowFunctions/CumulativeSalaryOfAnEmployee.sql
@@ -1,2 +1,22 @@
 -- LeetCode 579 – Find Cumulative Salary of an Employee
 
+SELECT 
+    Id,
+    `Month`,
+    Salary,
+FROM(
+    SELECT
+        Id,
+        `Month`,
+        SUM (Salary) OVER(
+            PARTITION BY Id,
+            ORDER BY `Month`,
+            ROWS BETWEEN 2 PRECEEDING AND CURRENT ROW
+        ) AS Salary,
+        MAX(`Month`) OVER(
+            PARTITION BY Id
+        ) AS MaxMonth
+    FROM Employee
+)t
+WHERE `Month`!=MaxMonth
+ORDER BY Id, `Month` DESC;
